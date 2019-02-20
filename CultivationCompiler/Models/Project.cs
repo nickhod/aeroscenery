83d505c17a20a2ad7c6c8cc89eff1e9971e6135c
Nick Hoddinott <nickh@conceptdelta.com>
2019-02-20 10:53:02 +0000

    Added CultivationCompiler and CultivationCompilerConsole

---
+++ CultivationCompiler/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using CultivationCompiler.Models.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CultivationCompiler.Models
+{
+    public class Project
+    {
+        public string MinimumCultivationCompilerVersion { get; set; }
+        public GeoData GeoData { get; set; }
+        public IList<Rule> Rules { get; set; }
+        public Output Output { get; set; }
+    }
+}
---
+++ CultivationCompiler/Services/ProjectService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using CultivationCompiler.Models;
+
+namespace CultivationCompiler.Services
+{
+    public class ProjectService
+    {
+        public async Task<Project> LoadProject(string filePath)
+        {
+            return await Task.Run(() =>
+            {
+                Project project = new Project();
+                XmlSerializer serializer = new XmlSerializer(typeof(Project));
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    project = (Project)serializer.Deserialize(reader);
+                    reader.Close();
+                }
+
+                serializer = null;
+                return project;
+            });
+
+
+        }
+
+    }
+}
